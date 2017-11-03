c290bcc69dc186bda831e865ef8e9a6597c4f2ca
dianavashti <diana.vashti@gmail.com>
2017-11-03 16:35:38 -0700

    Adds most of content and begins styling homepage

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', {title: "My Blog"})
+router.get('/', (reqest, response, next) => {
+  response.render('index', {title: "My Blog"})
 });
+
+router.get('/sample', (reqest, response, next) => {
+  response.render('sample')
+})
 
 module.exports = router;
